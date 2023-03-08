1e24be473fe5194e070a47b9168e7a055a5f6e47
Budanovvv <69916615+Budanovvv@users.noreply.github.com>
2023-03-08 11:40:31 +0200

    HW3 add work_with_operators.py

---
+++ hw_3/work_with_operators.py
@@ -0,0 +1,18 @@
+# comparison operators
+i = 1
+j = 3
+print(i == j)
+print(i != j)
+print(i <= j)
+print(i >= j)
+print(i < j)
+
+# int to str, and then concatenate it
+a = 2
+b = 5
+c = 6
+d = str(a) + str(b) + str(c)
+d_int = int(d)
+
+print(d)
+print(type(d))
