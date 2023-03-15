59f9e5603443f5a696376bd50f8f1d9ce93e3fd4
Budanovvv <69916615+Budanovvv@users.noreply.github.com>
2023-03-15 12:13:58 +0200

    HW4 Add to str_contain_digits_or_letters if data is digit check that digit is a double number

--- hw_4/str_contain_digits_or_letters.py
+++ hw_4/str_contain_digits_or_letters.py
@@ -1,11 +1,15 @@
 # Make function to compare data type of string
 def match_data_type(data):
     if data.isdigit():  # Try to find only digits
-        print("Your data contain only digits")
+        print("Data -> Contain only digits")
+        if int(data) % 2 == 0:  # Check that digit is a double number
+            print("Digit -> Double number")
+        else:
+            print("Digit -> Undouble number")
     elif data.isalpha():  # Try to find only letters
-        print("Your data contain only letters")
+        print("Data -> Contain only letters")
     elif data.isalnum():  # Try to find only letters and digits
-        print("Your data contain letters and digits")
+        print("Data -> Contain letters and digits")
     else:  # Exception if finding something other than letters and digits
         print(f"I don't expect this kind of data -> {data}")
 
